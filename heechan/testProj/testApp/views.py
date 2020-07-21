458c7c1f73c58d8be85c303a9ef0b8d48d6d8f69
kimheechan <kimhecan@gmail.com>
2020-07-21 15:57:44 +0900

    try auto

--- heechan/testProj/testApp/views.py
+++ heechan/testProj/testApp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from .crawling import crawler
+from .crawling import crawler2
 
 # Create your views here.
 
 
 def home(request):
-    arr = crawler.crawler()
+    arr = crawler2.crawler2()
     return render(request, 'home.html', {'arr': arr})

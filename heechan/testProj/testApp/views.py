f03e941797a33ce54b09d98f9fbd5d4c0e589594
kimheechan <kimhecan@gmail.com>
2020-07-27 14:48:29 +0900

    css 추가

--- heechan/testProj/testApp/views.py
+++ heechan/testProj/testApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .crawling import crawler2
+from .crawling import image
 
 # Create your views here.
 
@@ -7,11 +8,16 @@ from .crawling import crawler2
 def home(request):
     arr = crawler2.crawler2("https://freemusicarchive.org/static")
     arr1 = arr[0]
-    return render(request, 'home.html', {'arr': arr,'arr1' : arr1})
+    images = image.url_get()
+    image1 = images[0]
+    return render(request, 'home.html', {'arr': arr,'arr1' : arr1, 'genre': 'static', 'images': images, 'image1': image1 })
 
 def result(request):
     setting = request.GET.get('setting', '')
     url = "https://freemusicarchive.org/genre/" + setting
     arr = crawler2.crawler2(url)
     arr1 = arr[0]
-    return render(request, 'home.html',{'arr': arr,'arr1' : arr1})
+    images = image.url_get(setting)
+    image1 = images[0]
+
+    return render(request, 'home.html',{'arr': arr,'arr1' : arr1, 'genre': setting, 'images': images, 'image1': image1})

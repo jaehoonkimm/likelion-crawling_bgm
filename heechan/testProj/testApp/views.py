4ec28c6febef230ed5c15cdf3a75c26a10ecbca6
Gobans <audghks4452@gmail.com>
2020-07-31 23:44:12 +0900

    add artist,title

--- heechan/testProj/testApp/views.py
+++ heechan/testProj/testApp/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
 from .crawling import crawler2
+from .crawling import crawler3
 from .crawling import image
+import re
 
 # Create your views here.
 
 
 def home(request):
-    arr = crawler2.crawler2("https://freemusicarchive.org/static")
-    arr1 = arr[0]
+    arr,title,artist = crawler3.crawler3("https://freemusicarchive.org/static")
+    arr1,title1,artist1 = arr[0],title[0],artist[0]
     images = image.url_get()
     image1 = images[0]
-    return render(request, 'home.html', {'arr': arr,'arr1' : arr1, 'genre': 'static', 'images' : images, 'image1' : image1})
+    return render(request, 'home.html', {'arr': arr,'arr1' : arr1, 'genre': 'static', 'images': images, 'image1': image1,'title':title,'title1':title1,'artist':artist,'artist1':artist1})
 
 def result(request):
     setting = request.GET.get('setting', '')
     url = "https://freemusicarchive.org/genre/" + setting
-    arr = crawler2.crawler2(url)
-    arr1 = arr[0] if arr else ''
+    arr,title,artist = crawler2.crawler2(url)
+    arr1,title1,artist1 = arr[0] , title[0],artist[0]
     images = image.url_get(setting)
     image1 = images[0]
 
-    return render(request, 'home.html',{'arr': arr,'arr1' : arr1, 'genre': setting, 'images' : images, 'image1' : image1})
+    return render(request, 'home.html',{'arr': arr,'arr1' : arr1, 'genre': setting, 'images': images, 'image1': image1,'title':title,'title1':title1,'artist':artist,'artist1':artist1})

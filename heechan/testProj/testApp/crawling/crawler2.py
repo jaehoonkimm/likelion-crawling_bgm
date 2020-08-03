2fbeb19bebabb1508135fbe9d79fbe2de6cba4d3
Gobans <audghks4452@gmail.com>
2020-08-03 21:33:14 +0900

    fix favico, category issues

--- heechan/testProj/testApp/crawling/crawler2.py
+++ heechan/testProj/testApp/crawling/crawler2.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 def crawler2(url):
   r = requests.get(url)
@@ -7,12 +8,14 @@ def crawler2(url):
   soup = BeautifulSoup(html, 'html.parser')
 
   my_titles = soup.select('a.icn-arrow')
-  my_music = soup.select('.playtxt'> '.ptxt-track')
+  my_music = soup.select('.playtxt .ptxt-artist')
   my_artist = soup.select('.playtxt .ptxt-track')
 
   arr = []
   music_title = []
   music_artist= []
+  for i in my_titles:
+    arr.append(i.get('href'))
   for j in my_music:
     title_re = re.sub('(<([^>]+)>)',"",str(j))
     title_re  = title_re.replace('\n',"")
